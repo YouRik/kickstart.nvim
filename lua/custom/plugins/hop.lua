8af20930a1a20210be50fe2c8fddf64c625c9eae
Jurik Karimian <yourik@live.de>
2024-12-17 21:25:33 +0100

    Add hop keymap to jump to any char

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -9,18 +9,21 @@ return {
     local hop = require 'hop'
     hop.setup(opts)
     local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', '<leader><leader>s', function()
+      hop.hint_char1()
+    end, { remap = true, desc = 'Go to char in view' })
     vim.keymap.set('', '<leader><leader>f', function()
       hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = false }
-    end, { remap = true, desc = 'f but down the whole file' })
+    end, { remap = true, desc = 'f but down the whole view' })
     vim.keymap.set('', '<leader><leader>F', function()
       hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false }
-    end, { remap = true, desc = 'F but up the whole file' })
+    end, { remap = true, desc = 'F but up the whole view' })
     vim.keymap.set('', '<leader><leader>t', function()
       hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 }
-    end, { remap = true, desc = 't but down the whole file' })
+    end, { remap = true, desc = 't but down the whole view' })
     vim.keymap.set('', '<leader><leader>T', function()
       hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 }
-    end, { remap = true, desc = 'T but down the whole file' })
+    end, { remap = true, desc = 'T but down the whole view' })
     vim.keymap.set('', '<leader><leader>/', function()
       hop.hint_patterns { current_line_only = false }
     end, { remap = true, desc = 'Hop to pattern in file' })
