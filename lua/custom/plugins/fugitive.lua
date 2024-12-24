1ce04bc3d73ebddd43632f047f112650d676f6d1
Jurik Karimian <yourik@live.de>
2024-12-24 15:43:49 +0100

    Open fugitive to the right

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-fugitive',
 
   config = function()
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Open Git' })
+    vim.keymap.set('n', '<leader>gs', ':vertical rightbelow :Git<CR>', { desc = 'Open Git' })
     vim.keymap.set('n', '<leader>gl', ':Git log<CR>')
     vim.keymap.set('n', '<leader>gp', ':Git push<CR>')
   end,
