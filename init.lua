0ad8af200b793a0145efb8711030e9dd0aad702c
Jurik Karimian <yourik@live.de>
2024-12-29 11:36:59 +0100

    Add flutter tools

--- init.lua
+++ init.lua
@@ -701,12 +701,12 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>df',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
-        desc = '[F]ormat buffer',
+        desc = '[D]ocument [F]ormat buffer',
       },
     },
     opts = {
