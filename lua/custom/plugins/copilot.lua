8f0d2c9f73d1ca6dacc7246b399b5449e244774d
Jurik Karimian <yourik@live.de>
2025-01-05 18:11:37 +0100

    Disable copilot by default and invoke with control+c

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
   'github/copilot.vim',
+  config = function()
+    vim.cmd 'Copilot disable'
+    vim.keymap.set('i', '<C-c>', '<Plug>(copilot-suggest)', { noremap = true, silent = true })
+  end,
 }
