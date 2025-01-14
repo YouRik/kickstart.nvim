8d51bbbcb016ae1da4a9883077668cb3368250a9
Jurik Karimian <yourik@live.de>
2025-01-14 08:48:21 +0100

    Disable transparency

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -5,10 +5,7 @@ return {
     require('rose-pine').setup {
       dark_variant = 'main',
       extend_background_behind_borders = true,
-      dim_inactive_windows = false,
-      styles = {
-        transparency = true,
-      },
+      dim_inactive_windows = true,
     }
     vim.cmd 'colorscheme rose-pine'
   end,
